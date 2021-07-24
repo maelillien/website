0e8f23c0d07dc18f1463d2960122e5e8c1a1eb38
Mael Illien <mael.illien@hotmail.com>
2021-07-24 16:25:01 -0400

    another commit

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ from flask import Flask, render_template, request, redirect
 # Configure application
 app = Flask(__name__)
 
-
-
 @app.route('/index', methods=['POST', 'GET'])
 @app.route('/', methods=['POST', 'GET'])
 def index():
